916438ab659a33fa458abd64598d3810abff4c20
TejasriGunta <tejasri1.g@gmail.com>
2025-02-17 19:34:36 +0530

    set file watcher to none

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+st.set_option('server.fileWatcherType', 'none')
+
 from agents.fetch_news import fetch_news
 from agents.summarizaton import summarization
 from agents.sentiment import analyze_sentiment
